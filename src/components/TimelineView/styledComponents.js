dc6e864ff66ff943ca678be008633220b7b4cfbd
anushakamsetti <annu.honey97@gmail.com>
2023-07-07 08:45:58 +0000

    solution

--- src/components/TimelineView/styledComponents.js
+++ src/components/TimelineView/styledComponents.js
@@ -1,25 +1,25 @@
 import styled from 'styled-components/macro'
 
 export const TimelineContainer = styled.div`
-   display: flex;
-   justify-content: space-around;
-   align-items: center;
-   min-height: 100vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-height: 100vh;
 `
 
 export const ResponsiveContainer = styled.div`
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items: center;
-   max-width:1110px;
-   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1110px;
+  height: 100vh;
 `
 export const HeaderContainer = styled.div`
-   display: flex;
-   flex-direction: column;
-   align-items: center;
-   margin-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
 `
 export const Heading = styled.h1`
   text-align: center;
@@ -31,12 +31,13 @@ export const Heading = styled.h1`
   @media screen and (min-width: 768px) {
     font-size: 20px;
   }
+`
 
-  export const CCBPHeading = styled.span`
+export const CCBPHeading = styled.span`
   color: #2b237c;
   font-family: 'Roboto';
   font-weight: 500;
-  font-size:24px;
+  font-size: 24px;
   margin: 0px;
   @media screen and (min-width: 768px) {
     font-size: 30px;
